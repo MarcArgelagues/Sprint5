9734d9561f52144363c7e0bb25bd488c50689184
Marc Argelagués <marc.argelagues@gmail.com>
2023-02-09 21:12:16 +0100

    correcions

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function mostraAcudit() {
     response = await fetch('https://api.chucknorris.io/jokes/random');
     data = await response.json();
     document.getElementById("acudit2").innerHTML = data.value;
+   
   } else {
     response = await fetch('https://icanhazdadjoke.com/', {
       headers: {
@@ -22,8 +23,16 @@ async function mostraAcudit() {
     });
     data = await response.json();
     document.getElementById("acudit2").innerHTML = data.joke;
+
+ 
   }
+  if (reportAcudits.length>0) {
+  definitiuAcudits.push(reportAcudits)}
+  else{}
+ 
 }
+console.log(definitiuAcudits)
+
 
 function mostraBotons() {
   
@@ -37,28 +46,27 @@ function pushAcudit (joke, score) {
   reportAcudits=[]
     const date = new Date().toISOString();
     const dadaAcudit = { joke, score, date };
-    if (score === 0){
-      return;
+    if (score =false){
+      return reportAcudits=[];
     } else { 
       reportAcudits.push(dadaAcudit);
     }
-    console.log(reportAcudits)
+   
   }
 
   function temps(){ 
     fetch('https://www.el-tiempo.net/api/json/v2/home')
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+        //console.log(data);
         const barcelonaData = data.ciudades.find(ciudad => ciudad.name === 'Barcelona');
-        console.log(barcelonaData);
         if (barcelonaData) {
           const {temperatures, stateSky } = barcelonaData;
-          console.log(temperatures);
-          if(stateSky.description==="Intervalos nubosos"){
+          if(stateSky.description===stateSky.description){
           document.getElementById("meteo").innerHTML = `${temperatures.max}ºC | <img src="../sprint5/img/1821978.png">`;
           }
-          //A tall d'exemple. Altres descripcions anirien relacionades amb altres imatges i else ifs
+          //A tall d'exemple. Altres descripcions anirien relacionades amb altres imatges i else ifs canviant stateSkydescription
+          //per la descripció corresponent p.ex. "Intervalos Nubosos"
         } else {
           console.error("No s'han trobat dades per Barcelona");
         }
